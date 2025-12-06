7b92bb3759d07f34aa4099ad9f810fd854f0e5df
lutfihadiCEX <lutfihadij@gmail.com>
2025-12-06 21:00:35 +0800

    BM25 idx script.

--- src/bm25_indexer.py
+++ src/bm25_indexer.py
@@ -4,17 +4,19 @@ import pickle
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parent.parent
-METADATA_CSV = ROOT / 'data' / 'metadata.csv'
+METADATA_CSV = ROOT / 'indexes' / 'metadata.csv'  
 BM25_PATH = ROOT / 'indexes' / 'bm25.pkl'
 
-
-
 def build_bm25_index():
-
     metadata = pd.read_csv(METADATA_CSV)
 
-    # Tokenizing captions obtained from caption column in metadata.csv
-    corpus = metadata['caption'].fillna("").str.lower().str.split()
+    # BLIP generated caption
+    captions = metadata.get('generated_caption', None)
+    if captions is None:
+        captions = metadata['caption']
+
+    # Tokenizing captions
+    corpus = captions.fillna("").str.lower().str.split()
     bm25 = BM25Okapi(corpus)
 
     with open(BM25_PATH, 'wb') as f:
